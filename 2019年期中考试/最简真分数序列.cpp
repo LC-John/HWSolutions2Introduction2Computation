30e7f2516701fbd91e5a9cf54a43c808690dd671
zhanghz <zhang_hz@pku.edu.cn>
2020-11-10 20:48:49 +0800

    mid-2019

---
+++ 2019年期中考试/最简真分数序列.cpp
@@ -0,0 +1,60 @@
+/*****
+描述
+    所谓最简真分数是指分子小于分母，且不能约分的分数。给定 n（1 < n < 200）个非负整数（每个整数不超过3位），
+    哪两个整数分别作为分子和分母能够构成最简真分数。输出所有的最简真分数组合；若没有，则输出NO。
+关于输入
+    第1行为正整数个数n
+    其后为n个非负整数，空格间隔。
+关于输出
+    输出所有最简真分数，分子小的排在前面；若分子相同，分母小的排在前面。各分数之间用逗号间隔。
+    若没有，则输出NO
+例子输入
+    8
+    3 7 11 0 5 13 15 9
+例子输出
+    3/5,3/7,3/11,3/13,5/7,5/9,5/11,5/13,7/9,7/11,7/13,7/15,9/11,9/13,11/13,11/15,13/15
+*****/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX_N 1000
+
+int num[MAX_N] = {0}, n = 0;
+int prime[MAX_N] = {2, 3, 5, 7}, n_prime = 4;
+
+int main()
+{
+    bool first_output = true;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> num[i];
+    sort(num, num + n);
+    // for (int i = 0; i < n; i++)
+    //     cout << num[i] << " ";
+    // cout << endl;
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
+            {
+                bool flag = true;
+                if (num[i] == 0 || num[j] == 0)
+                    continue;
+                for (int f = 2;f <= num[i]; f++)
+                    if (num[i] % f == 0 && num[j] % f == 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+                if (flag)
+                {
+                    if (!first_output)
+                        cout << ",";
+                    cout << num[i] << "/" << num[j];
+                    first_output = false;
+                }
+            }
+    if (first_output)
+        cout << "NO";
+    return 0;
+}
---
+++ 2019年期中考试/谁能拿到最多的硬币.cpp
@@ -0,0 +1,49 @@
+/*****
+描述
+    有n个硬币排成一排，每个硬币上用一个数字标识了它的价值。每次要你从最左边或者最右边拿出一个硬币。总共拿k次，写一个算法，使能拿到的硬币的价值和最大。
+关于输入
+    输入包含两行，第一行为n， k；
+    第二行包含n个数字，表示n个硬币的价值。
+    1 <= k <= n <= 100000
+    单个硬币的价值大于0且不超过10000.
+关于输出
+    输出可以拿到的k个硬币最大的价值和。
+例子输入
+    6 3
+    5 4 3 2 1 6
+例子输出
+    15
+提示
+    从左边开始连续取两个硬币，右边取一个硬币即可。
+*****/
+
+#include <iostream>
+using namespace std;
+
+#define MAX_N 100010
+
+int val[MAX_N] = {0};
+
+int main()
+{
+    int n = 0, k = 0;
+    int sum_k = 0, min_sum_k = 0, sum = 0;
+    cin >> n >> k;
+    for (int i = 0; i < n - k; i++)
+    {
+        cin >> val[i];
+        sum_k += val[i];
+        min_sum_k += val[i];
+        sum += val[i];
+    }
+    for (int i = n - k; i < n; i++)
+    {
+        cin >> val[i];
+        sum += val[i];
+        sum_k += val[i] - val[i - (n - k)];
+        if (sum_k < min_sum_k)
+            min_sum_k = sum_k;
+    }
+    cout << sum - min_sum_k;
+    return 0;
+}
